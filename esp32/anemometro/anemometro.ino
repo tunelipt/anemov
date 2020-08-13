911a3e301f52415f0d91a6a4079b33a1b21906a8
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2020-08-13 17:08:49 -0300

    Nova interface de comunicação e possibilidade de usar wifi e serial para comunicar
    Até este momento o protocolo de comunicação especificava um comando para uma
    leitura. Agora é necessário especificar o tempo de aquisição em ms:
    
    ?1000!
    
    Aquisite durante 1000 ms.
    
    Além desta modificação, também implementei uma interface para wifi e outra
    para comunicação serial mesmo.
    
    Talvez eu mude de tempo para número de amostras. Vamos ver.

--- esp32/anemometro/anemometro.ino
+++ esp32/anemometro/anemometro.ino
@@ -20,14 +20,35 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
-#include "BluetoothSerial.h"
 
-//#define DEBUG_ANEM
+//#define USE_BLUETOOTH
+#define USE_WIFI
+
+#ifdef USE_BLUETOOTH
+#include "BluetoothSerial.h"
+#elif defined(USE_WIFI)
+#include <WiFi.h>
+#endif // USE_BLUETOOTH
+
+
 
 Adafruit_BMP280 bmp; // use I2C interface
 Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
 Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
-BluetoothSerial SerialBT;
+
+#ifdef USE_BLUETOOTH
+BluetoothSerial dev;
+#elif defined(USE_WIFI)
+WiFiServer dev(9590);
+const char *ssid = "tunel";
+const char *password = "tunelgvento";
+#endif
+
+
+const char *devname = "IPT0";
+
+
+
 
 const int AI_VEL = 34;
 const int AI_TEMP = 33;
@@ -55,14 +76,27 @@ void read_voltage(){
     aivals[k] /= NSAMPLES;
     }
 }
+
+void read_anem(int *vel, int *temp, float *tref, float *Pa){
+  read_voltage();
+
+  *vel = aivals[0];
+  *temp = aivals[1];
+  
+  sensors_event_t temp_event, pressure_event;
+  bmp_temp->getEvent(&temp_event);
+  bmp_pressure->getEvent(&pressure_event);
+
+  *tref = temp_event.temperature;
+  *Pa = pressure_event.pressure/10;
+}
+
+
 void setup() {
+
+
   
-  SerialBT.begin("IPTANEM0");
-  
-#ifdef DEBUG_ANEM  
   Serial.begin(9600);
-#endif //DEBUG_ANEM
-  
   if (!bmp.begin(0x76)) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1) delay(10);
@@ -76,51 +110,143 @@ void setup() {
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
   //bmp_temp->printSensorDetails();
+#ifdef USE_BLUETOOTH
+  dev.begin("IPTANEM0");
+#elif defined(USE_WIFI)
+  Serial.print("Conectando-se a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Conectado ao WiFi");
+  Serial.print("Endereço de IP: ");
+  Serial.println(WiFi.localIP());
+  
+  dev.begin();
+#endif
+  
 }
+
+
+int parse_header(String hdr){
+  int idx1, idx2;
+  idx1 = hdr.indexOf("?");
+  idx2 = hdr.indexOf("!");
+
+  if (idx1 < 0 || idx2 < 0){
+    return -1;
+  }
+  String ss = hdr.substring(idx1+1, idx2);
+  int ttot = ss.toInt();  
+  return ttot;
+}
+
+#ifdef USE_BLUETOOTH
 
 void loop(){
-  int idx;
-  int nsamples;
-  if (SerialBT.available()){
-    delay(10);
-    
-    btresp = SerialBT.readString();
-    idx = btresp.indexOf("?");
-    if (idx > -1){
-      // Ler o número de amostras:
-      read_voltage();
-      sensors_event_t temp_event, pressure_event;
-      bmp_temp->getEvent(&temp_event);
-      bmp_pressure->getEvent(&pressure_event);
+
+  int t1, dt, ttot;
+  String hdr;
+  int vel, temp;
+  float tref, Pa;
   
-      SerialBT.println("BEGIN");
-      SerialBT.println(aivals[0]); 
-      SerialBT.println(aivals[1]); 
-      SerialBT.println(temp_event.temperature);
-      SerialBT.println(pressure_event.pressure/10);
-      SerialBT.println("END");
+  if (dev.available()){
+    delay(20);
+    hdr = dev.readString();
+    ttot = parse_header(hdr);
+    t1 = millis();
+    dt = 0;
+    while(dt < ttot){
+      read_anem(&vel, &temp, &tref, &Pa);
+      dt = millis() - t1;
+      dev.print(devname); dev.print(",");
+      dev.print(dt); dev.print(",");
+      dev.print(vel); dev.print(",");
+      dev.print(temp); dev.print(",");
+      dev.print(tref); dev.print(",");
+      dev.print(Pa); dev.println(",END");
+      if (dt > ttot) 
+        break;
+       
     }
-  }
-
-#ifdef DEBUG_ANEM
-  if (Serial.available()){
-    btresp = Serial.readString();
-    if (btresp.indexOf("?") > -1){
   
-      read_voltage();
-      sensors_event_t temp_event, pressure_event;
-      bmp_temp->getEvent(&temp_event);
-      bmp_pressure->getEvent(&pressure_event);
-
-      Serial.println("BEGIN");
-      Serial.println(aivals[0]);
-      Serial.println(aivals[1]);
-      Serial.println(temp_event.temperature);
-      Serial.println(pressure_event.pressure/10);
-      Serial.println("END");
-    }
   }
-
-  delay(10);
-#endif // DEBUG_ANEM
 }
+
+
+#elif defined(USE_WIFI)
+
+WiFiClient dev1;
+void loop(){
+
+  Serial.println("Estamos no wifi loop"); 
+  int t1, dt, ttot;
+  
+  String hdr;
+  int vel, temp;
+  float tref, Pa;
+  if (!dev1){
+    dev1= dev.available();
+  }else{
+    if (dev1.available()){
+      delay(20);
+      hdr = dev1.readString();
+      ttot = parse_header(hdr);
+      t1 = millis();
+      dt = 0;
+      while(dt < ttot){
+        read_anem(&vel, &temp, &tref, &Pa);
+        dt = millis() - t1;
+        dev1.print(devname); dev1.print(",");
+        dev1.print(dt); dev1.print(",");
+        dev1.print(vel); dev1.print(",");
+        dev1.print(temp); dev1.print(",");
+        dev1.print(tref); dev1.print(",");
+        dev1.print(Pa); dev1.println(",END");
+        if (dt > ttot) 
+          break;
+      }
+    }
+  }
+  
+}
+
+
+
+#else
+
+void loop(){
+
+  int t1, dt, ttot;
+  String hdr;
+  int vel, temp;
+  float tref, Pa;
+  
+  if (Serial.available()){
+    delay(20);
+    hdr = Serial.readString();
+    ttot = parse_header(hdr);
+    t1 = millis();
+    dt = 0;
+    while(dt < ttot){
+      read_anem(&vel, &temp, &tref, &Pa);
+      dt = millis() - t1;
+      Serial.print(devname); Serial.print(",");
+      Serial.print(dt); Serial.print(",");
+      Serial.print(vel); Serial.print(",");
+      Serial.print(temp); Serial.print(",");
+      Serial.print(tref); Serial.print(",");
+      Serial.print(Pa); Serial.println(",END");
+      if (dt > ttot) 
+        break;
+    }
+  
+  }
+}
+
+
+#endif
